378e79d1bc96d2ec821adc9ff5315602267083e9
Krishna0306515 <krishnakrishna59811@gmail.com>
2026-08-20 16:10:01 +0530

    Add files via upload

---
+++ studentlist.js
@@ -0,0 +1,11 @@
+let student=new Object();
+student.name="vokas";
+student.id="3993;"
+student.city="ghaziabad";
+student.learning=function(){
+    console.log("learning tech courses");
+    console.log(student.name);
+    student.learning();
+
+}
+
